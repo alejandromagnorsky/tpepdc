b85b4dfc2ad87160600aab13c9c99816b89ec1b7
alejandromagnorsky <alejandromagnorsky@b07b898e-bdeb-e7c2-b049-e3c4f0fd2394>
2011-05-30 22:06:24 +0000

    Ya hay un proxy que interpreta los comandos POP3 y se comunica por default con un servidor POP3 de Yahoo usando al POP3Client

---
+++ src/Client.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+public abstract class Client extends SocketUser{
+
+	private static final SSLSocketFactory SocketFactory = (SSLSocketFactory) SSLSocketFactory
+			.getDefault();
+	
+	public void connect(String host, int port) throws IOException {
+        socket = (SSLSocket)SocketFactory.createSocket();
+        socket.connect(new InetSocketAddress(host, port));
+        reader = new BufferedReader(new InputStreamReader(socket
+				.getInputStream()));
+        // Enable auto-flush after println
+        writer = new PrintWriter(new OutputStreamWriter(socket
+				.getOutputStream()), true);
+		if (debug)
+			System.out.println("Connected to the host");
+	}
+
+	
+}
