cb47dfbcaa2033ca6b2c11a95df3e9f11bdfb938
alejandromagnorsky <alejandromagnorsky@b07b898e-bdeb-e7c2-b049-e3c4f0fd2394>
2011-06-10 23:28:32 +0000

    Principio del processBody

--- trunk/src/POP3Client.java
+++ trunk/src/POP3Client.java
@@ -4,9 +4,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.imageio.ImageIO;
+
 public class POP3Client extends Client {
 
-	private static final int PORT = 995; //TODO
+	private static final int PORT = 995; // TODO
 
 	public void connect(String host) throws IOException {
 		connect(host, PORT);
@@ -15,7 +17,7 @@ public class POP3Client extends Client {
 	}
 
 	protected String readResponseLine() throws IOException {
-		String response = reader.readLine();		
+		String response = reader.readLine();
 		logger.info("[in] : " + response);
 		return response;
 	}
@@ -39,11 +41,11 @@ public class POP3Client extends Client {
 		while ((response = readResponseLine()).length() != 0) {
 			if (response.startsWith("\t"))
 				continue;
-			
+
 			int separator = response.indexOf(":");
-			if(separator == -1)
+			if (separator == -1)
 				continue;
-			
+
 			headerName = response.substring(0, separator);
 			if (response.length() > separator)
 				headerValue = response.substring(separator + 2);
@@ -59,11 +61,9 @@ public class POP3Client extends Client {
 		}
 
 		// Between the header and the body there is a \n
-		// Process body
-		StringBuilder bodyBuilder = new StringBuilder();
-		while (!(response = readResponseLine()).equals("."))
-			bodyBuilder.append(response + "\n");
-		return new Message(headers, bodyBuilder.toString());
+		Message message = new Message(headers);
+		processBody(message);
+		return message;
 	}
 
 	public List<Message> getMessages() throws IOException {
@@ -71,7 +71,61 @@ public class POP3Client extends Client {
 		List<Message> messages = new ArrayList<Message>();
 		for (int i = 1; i <= quant; i++)
 			messages.add(getMessage(i));
-		
+
 		return messages;
+	}
+
+	private void processBody(Message message) throws IOException {
+		Content content;
+		String response, boundary, contentTypeHeader, type;
+		contentTypeHeader = message.getHeaders().get("Content-Type").get(0);
+		boundary = contentTypeHeader
+				.substring(contentTypeHeader.indexOf("=") + 1);
+
+		StringBuilder bodyBuilder = new StringBuilder(), contentText;
+		while (!(response = readResponseLine()).equals(".")) {
+			bodyBuilder.append(response + "\n");
+
+			if (response.equals("--" + boundary)) {
+				response = readResponseLine();
+
+				if (response.contains("Content-Type:")) {
+					contentTypeHeader = response.substring(response
+							.indexOf(':') + 2);
+					type = contentTypeHeader
+							.substring(0, response.indexOf('/'));
+					if (type.equals("text"))
+						content = new TextContent(contentTypeHeader);
+					else if (type.equals("image"))
+						content = new ImageContent(contentTypeHeader);
+					else
+						content = new OtherContent(contentTypeHeader);
+
+					do
+						bodyBuilder.append(response + "\n");
+					while ((response = readResponseLine()).length() != 0);
+					bodyBuilder.append(response + "\n");
+
+					contentText = new StringBuilder();
+					while (!(response = readResponseLine()).contains("--" + boundary)){
+						bodyBuilder.append(response + "\n");
+						contentText.append(response);
+					}
+								
+					if (type.equals("text"))
+						((TextContent)content).setText(contentText.toString());
+					else if (type.equals("image"))
+						((ImageContent)content).setImage(base64ToImage(contentText.toString()));
+					else
+						content = new OtherContent(contentTypeHeader);
+				}
+
+			}
+		}
+		message.setBody(bodyBuilder.toString());
+	}
+
+	private ImageIO base64ToImage(String base64String) {
+		return null;
 	}
 }
