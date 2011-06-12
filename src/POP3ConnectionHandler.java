c547e353f679dad6803c435bfb64a1c89261f210
alejandromagnorsky <alejandromagnorsky@b07b898e-bdeb-e7c2-b049-e3c4f0fd2394>
2011-06-12 16:13:41 +0000

    Detalles

--- src/POP3ConnectionHandler.java
+++ src/POP3ConnectionHandler.java
@@ -14,17 +14,16 @@ public class POP3ConnectionHandler extends ConnectionHandler {
 	// TODO cargar el usuario, la blacklist y los loginsPerDay
 	private User user;
 	private List<String> ipBlackList;
-	
-	//TODO cambiar esto cuando el loader sea un singleton
-	private XMLSettingsDAO loader = new XMLSettingsDAO("settings.xml", "settings.xsd");
-	 
+
+	private XMLSettingsDAO loader = new XMLSettingsDAO("settings.xml", "src/settings.xsd");
+
 	public POP3ConnectionHandler(Socket socket) {
 		super(socket);
-		//TODO el load deberia estar aca?
+		// TODO el load deberia estar aca?
 		try {
 			loader.load();
 		} catch (Exception e) {
-			//TODO
+			// TODO
 		}
 		this.POP3client = new POP3Client();
 		this.ipBlackList = loader.getBlacklistIP();
@@ -32,14 +31,15 @@ public class POP3ConnectionHandler extends ConnectionHandler {
 
 	public void run() {
 		try {
-			
 			String request, response;
 
 			do {
 				request = reader.readLine();
 				if (!request.toUpperCase().contains("PASS"))
 					request = request.toUpperCase();
-				if(request.startsWith("USER ")) {
+				
+				if (request.contains("USER ") && !POP3client.isConnected()) {
+					/*
 					loadUser(request.substring(request.lastIndexOf(' ') + 1));
 					
 					String userServer = user.getSettings().getServer();
@@ -50,17 +50,26 @@ public class POP3ConnectionHandler extends ConnectionHandler {
 					} else {
 						server = userServer;
 					}
-					
-					//TODO borrar la siguiente linea cuando arregle el resto
+
+					// TODO borrar la siguiente linea cuando arregle el resto
 					server = DEFAULT_SERVER;
-					
-					POP3client.connect(server);
-					
-					if(accessIsDenied(ip)) {
-						request = "QUIT";
-					}
+
+					if (accessIsDenied(ip)) {
+						response = "-ERR. Access denied.";
+						writer.println(response);
+						if(POP3client.isConnected()){
+							request = "QUIT";
+							POP3client.send(request);
+						}												
+					} else */
+						POP3client.connect(DEFAULT_SERVER);
 				}
-				response = POP3client.send(request);
+				
+				if(POP3client.isConnected())
+					response = POP3client.send(request);					
+				else 
+					response = "-ERR. Must use USER command first.";
+				
 				writer.println(response);
 				if (request.contains("RETR") && response.contains("+OK")) {
 					Message message = POP3client.getMessage();
@@ -69,8 +78,9 @@ public class POP3ConnectionHandler extends ConnectionHandler {
 				}
 
 			} while (isConnected() && !request.contains("QUIT"));
-
-			POP3client.disconnect();
+			
+			if(POP3client.isConnected())
+				POP3client.disconnect();
 			disconnect();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -84,34 +94,35 @@ public class POP3ConnectionHandler extends ConnectionHandler {
 		for (Filter f : filters)
 			f.apply(message);
 	}
-	
+
 	private boolean accessIsDenied(String ip) {
-		if(AccessControl.exceedsMaxLogins(user)) {
-			writer.println("ERROR. You have exceeded the ammount of " +
-					"logins for today. Please try again tomorrow");
+		if (AccessControl.exceedsMaxLogins(user)) {
+			writer.println("ERROR. You have exceeded the ammount of "
+					+ "logins for today. Please try again tomorrow");
 			return true;
 		}
-		
-		if(AccessControl.hourIsOutOfRange(user)) {
-			writer.println("ERROR. You are not allowed to login now. Try again" +
-					"between " + user.getSettings().getSchedule().getFrom() + 
-					"and " + user.getSettings().getSchedule().getTo() + "hs");
+
+		if (AccessControl.hourIsOutOfRange(user)) {
+			writer.println("ERROR. You are not allowed to login now. Try again"
+					+ "between " + user.getSettings().getSchedule().getFrom()
+					+ "and " + user.getSettings().getSchedule().getTo() + "hs");
 			return true;
 		}
-		
-		if(AccessControl.ipIsDenied(ipBlackList, ip)) {
+
+		if (AccessControl.ipIsDenied(ipBlackList, ip)) {
 			writer.println("ERROR. Login failed");
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	private void loadUser(String user) {
 		List<User> users = loader.getUserList();
-		for(User u : users) {
-			if(user.toUpperCase().equals(u.getName().toUpperCase())) {
+		for (User u : users) {
+			if (user.toUpperCase().equals(u.getName().toUpperCase())) {
 				this.user = u;
+				return;
 			}
 		}
 	}
