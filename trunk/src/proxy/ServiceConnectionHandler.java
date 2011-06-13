6761c0325fd426747319614b335a105c1983c247
alejandromagnorsky <alejandromagnorsky@b07b898e-bdeb-e7c2-b049-e3c4f0fd2394>
2011-06-13 01:28:52 +0000

    ServiceConnectionHandler

---
+++ trunk/src/proxy/ServiceConnectionHandler.java
@@ -0,0 +1,49 @@
+package proxy;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import model.Administrator;
+
+import dao.XMLAdminDAO;
+
+public abstract class ServiceConnectionHandler extends ConnectionHandler {
+
+	private XMLAdminDAO adminDAO = new XMLAdminDAO("admin.xml", "src/admin.xsd");
+
+	public ServiceConnectionHandler(Socket socket) {
+		super(socket);
+		try {
+			adminDAO.load();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected boolean validateLogin() {
+		String request;
+		boolean login = false;
+		try {
+			do {
+				request = reader.readLine();
+				if (!request.toUpperCase().contains("LOGIN"))
+					writer.println("ERROR. Login first");
+				else
+					login = true;
+			} while (!login);
+		} catch (IOException e) {
+			return false;
+		}
+		
+		String[] args = request.split(" ");
+		if (args.length != 3)
+			return false;
+		String name = args[1];
+		String password = args[2];
+		Administrator admin = adminDAO.getAdministrator(name);
+		if (admin == null)
+			return false;
+		return admin.verifyPassword(password);
+	}
+
+}
--- trunk/src/statistics/StatisticsConnectionHandler.java
+++ trunk/src/statistics/StatisticsConnectionHandler.java
@@ -3,15 +3,14 @@ package statistics;
 import java.io.IOException;
 import java.net.Socket;
 
+import model.User;
+import proxy.ServiceConnectionHandler;
 import dao.XMLSettingsDAO;
 
-import proxy.ConnectionHandler;
+public class StatisticsConnectionHandler extends ServiceConnectionHandler {
 
-import model.User;
-
-public class StatisticsConnectionHandler extends ConnectionHandler {
-
-	private XMLSettingsDAO loader = new XMLSettingsDAO("settings.xml", "src/settings.xsd");
+	private XMLSettingsDAO loader = new XMLSettingsDAO("settings.xml",
+			"src/settings.xsd");
 
 	public StatisticsConnectionHandler(Socket socket) {
 		super(socket);
@@ -26,59 +25,66 @@ public class StatisticsConnectionHandler extends ConnectionHandler {
 		try {
 			String request, response;
 			User user = null;
-			do {
-				request = reader.readLine().toUpperCase();
 
-				if (request.startsWith("USER ")) {
-					// TODO Obtener al user del XML
-					String username = request.substring(request
-							.lastIndexOf(' ') + 1);
-					user = loader.getUser(username);
-					if (user == null)
-						response = "ERROR. User " + username
-								+ " doesn't exists";
+			if (validateLogin()) {
+				writer.println("OK. Welcome to the statistics service");
+				do {
+					request = reader.readLine().toUpperCase();
+
+					if (request.startsWith("USER ")) {
+						String username = request.substring(request
+								.lastIndexOf(' ') + 1);
+						user = loader.getUser(username);
+						if (user == null)
+							response = "ERROR. User " + username
+									+ " doesn't exists";
+						else
+							response = "OK. Now the statistics are for the user "
+									+ user.getName();
+					} else if (request.equals("PUBLIC")) {
+						user = null;
+						response = "OK. Now the statistics are public";
+					} else if (request.equals("AQ"))
+						response = "OK. Access quant: "
+								+ ((user == null) ? Statistics.getAccessQuant()
+										: Statistics.getAccessQuant(user));
+					else if (request.equals("BT"))
+						response = "OK. Bytes transfered: "
+								+ ((user == null) ? Statistics
+										.getBytesTransfered() : Statistics
+										.getBytesTransfered(user));
+					else if (request.equals("RQ"))
+						response = "OK. Red quant: "
+								+ ((user == null) ? Statistics.getRedQuant()
+										: Statistics.getRedQuant(user));
+					else if (request.equals("DQ"))
+						response = "OK. Deleted quant: "
+								+ ((user == null) ? Statistics
+										.getDeletedQuant() : Statistics
+										.getDeletedQuant(user));
+					else if (request.equals("AH")) {
+						StringBuilder ans = new StringBuilder();
+						ans.append("OK. Access histogram:\n");
+						for (Statistics.Access access : (user == null) ? Statistics
+								.getAccessHistogram()
+								: Statistics.getAccessHistogram(user))
+							ans.append("Date: "
+									+ access.getDate().getDayOfMonth() + "/"
+									+ access.getDate().getMonthOfYear() + "/"
+									+ access.getDate().getYear()
+									+ " - Access quant: " + access.getQuant()
+									+ "\n");
+						response = ans.toString();
+					} else if (request.equals("EXIT"))
+						response = "OK.";
 					else
-						response = "OK. Now the statistics are for the user "
-								+ user.getName();
-				} else if (request.equals("PUBLIC")) {
-					user = null;
-					response = "OK. Now the statistics are public";
-				} else if (request.equals("AQ"))
-					response = "OK. Access quant: "
-							+ ((user == null) ? Statistics.getAccessQuant()
-									: Statistics.getAccessQuant(user));
-				else if (request.equals("BT"))
-					response = "OK. Bytes transfered: "
-							+ ((user == null) ? Statistics.getBytesTransfered()
-									: Statistics.getBytesTransfered(user));
-				else if (request.equals("RQ"))
-					response = "OK. Red quant: "
-							+ ((user == null) ? Statistics.getRedQuant()
-									: Statistics.getRedQuant(user));
-				else if (request.equals("DQ"))
-					response = "OK. Deleted quant: "
-							+ ((user == null) ? Statistics.getDeletedQuant()
-									: Statistics.getDeletedQuant(user));
-				else if (request.equals("AH")) {
-					StringBuilder ans = new StringBuilder();
-					ans.append("OK. Access histogram:\n");
-					for (Statistics.Access access : (user == null) ? Statistics
-							.getAccessHistogram() : Statistics
-							.getAccessHistogram(user))
-						ans.append("Date: " + access.getDate().getDayOfMonth()
-								+ "/" + access.getDate().getMonthOfYear() + "/"
-								+ access.getDate().getYear()
-								+ " - Access quant: " + access.getQuant()
-								+ "\n");
-					response = ans.toString();
-				} else if (request.equals("EXIT"))
-					response = "OK.";
-				else
-					response = "ERROR. Invalid command";
+						response = "ERROR. Invalid command";
 
-				writer.println(response);
-			} while (isConnected() && !request.equals("EXIT"));
-
+					writer.println(response);
+				} while (isConnected() && !request.equals("EXIT"));
+			} else {
+				writer.println("ERROR. User or password incorrect");
+			}
 			disconnect();
 		} catch (IOException e) {
 			e.printStackTrace();
