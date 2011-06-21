cff74607135ec87e7ecbb1abe0aa4cb3f1d83882
merchante.mariano <merchante.mariano@b07b898e-bdeb-e7c2-b049-e3c4f0fd2394>
2011-06-21 13:14:35 +0000

    Imagen

--- trunk/src/main/java/imageTest/ImageTest.java
+++ trunk/src/main/java/imageTest/ImageTest.java
@@ -1,16 +1,23 @@
 package imageTest;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.imageio.ImageIO;
 
 import org.apache.commons.codec.binary.Base64;
+
+import com.sun.xml.internal.messaging.saaj.packaging.mime.internet.MimeUtility;
 
 public class ImageTest {
 
@@ -24,41 +31,75 @@ public class ImageTest {
 			while ((temp = reader.readLine()) != null) {
 				imageString += temp;
 			}
-//			BufferedImage image = base64ToImage(imageString);
+
+			InputStream s = new ByteArrayInputStream(imageString.getBytes());
+			InputStream decoder = MimeUtility.decode(s, "base64");
+
+			String tmp = "";
+			int c;
+			while ((c = decoder.read()) != -1)
+				tmp += (char) c;
+
+			byte[] imageInBytes = tmp.getBytes();
+			BufferedImage image = ImageIO.read(new ByteArrayInputStream(
+					imageInBytes));
+
+			BufferedImage transformed = new BufferedImage(image.getWidth(),
+					image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+			Graphics g = transformed.getGraphics();
+			Graphics2D graphics = (Graphics2D) g;
+			graphics.rotate(Math.PI, image.getWidth() / 2,
+					image.getHeight() / 2);
+			graphics.drawImage(image, 0, 0, null);
+
+			ImageIO.write(transformed, "png", new File("transformada.png"));
+
+			OutputStream out = new FileOutputStream("image_encodeada.txt");
+			OutputStream encoder = MimeUtility.encode(out, "base64");
+
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ImageIO.write(transformed, "PNG", baos);
+			byte[] transfImageBytes = baos.toByteArray();
+
+			encoder.write(transfImageBytes);
+
+			// BufferedImage image = base64ToImage(imageString);
 			// ImageTransformerFilter rotate = new ImageTransformerFilter();
 			// rotate.apply(message, dao.getUser("tpepdc"),
 			// new NullResponseFilter());
-//			ImageIO.write(image, "png", new File("TallerSistemasRep.png"));
-			
-			System.out.println("el string de image.txt es base 64?: " + Base64.isBase64(imageString));
-			
-			byte[] image = Base64.decodeBase64(imageString.getBytes());
-			
-//			System.out.println(new String(Base64.encodeBase64Chunked(image)));
-			
-			
-			BufferedImage buff = ImageIO.read(new ByteArrayInputStream(image));
-			
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ImageIO.write(buff, "png", baos);
-			byte[] buf = baos.toByteArray();
-			System.out.println(new String(Base64.encodeBase64Chunked(buf)));
-			
-			
-//			BufferedImage im = ImageIO.read(new File("image.png"));
-//			
-//
-//			String toPrint = imageToBase64(im, "png");
-//			BufferedImage asiQueda = base64ToImage(toPrint);
-//			ImageIO.write(asiQueda, "png", new File("asiQueda.png"));
-//			
-//			String ultima = imageToBase64(asiQueda, "png");
-//			
-//			System.out.println("original:     " + imageString);
-//			System.out.println("transformada: " + toPrint);
-//			System.out.println("ultima:       " + ultima);
-//			System.out.println("son iguales?: " + imageString.equals(toPrint));
-			
+			// ImageIO.write(image, "png", new File("TallerSistemasRep.png"));
+
+			// System.out.println("el string de image.txt es base 64?: "
+			// + Base64.isBase64(imageString));
+			//
+			// byte[] image = Base64.decodeBase64(imageString.getBytes());
+
+			// System.out.println(new
+			// String(Base64.encodeBase64Chunked(image)));
+			//
+			// BufferedImage buff = ImageIO.read(new
+			// ByteArrayInputStream(image));
+			//
+			// ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			// ImageIO.write(buff, "png", baos);
+			// byte[] buf = baos.toByteArray();
+			// System.out.println(new String(Base64.encodeBase64Chunked(buf)));
+
+			// BufferedImage im = ImageIO.read(new File("image.png"));
+			//			
+			//
+			// String toPrint = imageToBase64(im, "png");
+			// BufferedImage asiQueda = base64ToImage(toPrint);
+			// ImageIO.write(asiQueda, "png", new File("asiQueda.png"));
+			//			
+			// String ultima = imageToBase64(asiQueda, "png");
+			//			
+			// System.out.println("original:     " + imageString);
+			// System.out.println("transformada: " + toPrint);
+			// System.out.println("ultima:       " + ultima);
+			// System.out.println("son iguales?: " +
+			// imageString.equals(toPrint));
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
