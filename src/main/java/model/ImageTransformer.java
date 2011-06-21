2e633a979fcfb706a7ade36642297fc20f49eaff
jimenabpose <jimenabpose@b07b898e-bdeb-e7c2-b049-e3c4f0fd2394>
2011-06-21 14:38:45 +0000

    solo imagenes png

--- src/main/java/model/ImageTransformer.java
+++ src/main/java/model/ImageTransformer.java
@@ -3,84 +3,68 @@ package model;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringReader;
 
 import javax.imageio.ImageIO;
 
-import org.apache.commons.codec.binary.Base64;
+import org.apache.log4j.Logger;
+
+import com.sun.xml.internal.messaging.saaj.packaging.mime.util.BASE64DecoderStream;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.util.BASE64EncoderStream;
 
 public class ImageTransformer {
+	
+	public static Logger logger = Logger.getLogger("logger");
 
-	public String transform(String imageString) {
-		// if (user != null && user.getSettings() != null
-		// && user.getSettings().isRotate() != null
-		// && user.getSettings().isRotate())
+	public String transform(String imageStringInput) {
 
-		// POP3Proxy.logger.info(c.getContentTypeHeader());
-
-		BufferedImage image = base64ToImage(imageString);
-		BufferedImage out = new BufferedImage(image.getWidth(), image
-				.getHeight(), BufferedImage.TYPE_INT_ARGB);
-
-		Graphics g = out.getGraphics();
-		Graphics2D graphics = (Graphics2D) g;
-		graphics.rotate(Math.PI, image.getWidth() / 2, image.getHeight() / 2);
-		graphics.drawImage(image, 0, 0, null);
-
-		// TODO obtener el format
-		return imageToBase64(out, "");
-	}
-
-	public BufferedImage base64ToImage(String base64String) {
 		try {
-			byte[] imageInBytes = Base64.decodeBase64(base64String);
-			return ImageIO.read(new ByteArrayInputStream(imageInBytes));
+			BufferedReader reader = new BufferedReader(new StringReader(
+					imageStringInput));
+			String imageString = "";
+			String temp;
+			while ((temp = reader.readLine()) != null) {
+				imageString += temp;
+			}
+
+			InputStream s = new ByteArrayInputStream(imageString.getBytes());
+			InputStream decoder = new BASE64DecoderStream(s);
+
+//			String tmp = "";
+//			int c;
+//			while ((c = decoder.read()) != -1)
+//				tmp += (char) c;
+//
+//			byte[] imageInBytes = tmp.getBytes("iso-8859-1");
+//			BufferedImage image = ImageIO.read(new ByteArrayInputStream(
+//					imageInBytes));
+			BufferedImage image = ImageIO.read(decoder);
+
+			BufferedImage transformed = new BufferedImage(image.getWidth(),
+					image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+			Graphics g = transformed.getGraphics();
+			Graphics2D graphics = (Graphics2D) g;
+			graphics.rotate(Math.PI, image.getWidth() / 2,
+					image.getHeight() / 2);
+			graphics.drawImage(image, 0, 0, null);
+
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			OutputStream encoder = new BASE64EncoderStream(out);
+
+			ImageIO.write(transformed, "PNG", encoder);
+
+			byte[] buf = out.toByteArray();
+			return new String(buf);
 		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
+			logger.fatal("Error transforming image, returning original one.");
 		}
+		return imageStringInput;
 	}
 
-	// Format: png,jpg etc
-	private String imageToBase64(BufferedImage image, String format) {
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		try {
-			ImageIO.write(image, format, baos);
-			byte[] buf = baos.toByteArray();
-			return Base64.encodeBase64String(buf);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	// ORIGINAL
-
-	// public void apply(Message message, User user, ResponseFilter chain) {
-	// if (user != null && user.getSettings() != null
-	// && user.getSettings().isRotate() != null
-	// && user.getSettings().isRotate())
-	// for (Content c : message.getContents()) {
-	// if (c.getType().equals(Content.Type.IMAGE)) {
-	//
-	// POP3Proxy.logger.info(c.getContentTypeHeader());
-	// ImageContent iContent = (ImageContent) c;
-	//
-	// BufferedImage image = iContent.getImage();
-	// BufferedImage out = new BufferedImage(image.getWidth(),
-	// image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-	//
-	// Graphics g = out.getGraphics();
-	// Graphics2D graphics = (Graphics2D) g;
-	// graphics.rotate(Math.PI, image.getWidth() / 2, image
-	// .getHeight() / 2);
-	// graphics.drawImage(image, 0, 0, null);
-	//
-	// iContent.setImage(out);
-	// }
-	// }
-	// chain.doFilter(message, user);
-	// }
 }
